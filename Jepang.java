aac3dab7670fc16196a280bf22f6007f7e1e212d
unknown <indaherika09@gmail.com>
2014-10-18 17:30:04 +0700

    commit01

--- Jepang.java
+++ Jepang.java
@@ -1,58 +1,57 @@
-class Jepang{
-	String Makanan;
-	String Pakaian;
-	String Bunga;
-	String Senjata;
-	
-	
+class Jepang{ //nama kelas
+//mendeklarasikan variabel
+	String Makanan; //mendeklarasikan variabel Makanan dengan tipe data string
+	String Pakaian; //mendeklarasikan variabel Pakaian dengan tipe data string
+	String Bunga; //mendeklarasikan variabel Bunga dengan tipe data string
+	String Senjata; //mendeklarasikan variabel Senjata dengan tipe data string
+
 	
 	//Atribut
-	public String getMakanan(){
-		return Makanan;
+	public String getMakanan(){ //menginisialisasi nilai Makanan
+		return Makanan; //mengembalikan nilai makanan
 	}
-	public void setMakanan(String a){
+	public void setMakanan(String a){ //mendeklarasikan set untuk atribut makanan
 		this.Makanan=a;
 	}
 	
-	public String getPakaian(){
-		return Pakaian;
+	public String getPakaian(){ //menginisialisasi nilai Pakaian
+		return Pakaian; //mengembalikan nilai Pakaian
+
 	}
-	public void setPakaian(String b){
+	public void setPakaian(String b){ //mendeklarasikan set untuk atribut Pakaian
 		this.Pakaian=b;
 	}
 	
-	public String getBunga(){
-		return Bunga;
+	public String getBunga(){ //menginisialisasi nilai Bunga
+		return Bunga; //mengembalikan nilai Bunga
 	}
-	public void setBunga(String c){
+	public void setBunga(String c){ //mendeklarasikan set untuk atribut Bunga
 		this.Bunga=c;
 	}
 	
-	public String getSenjata(){
-		return Senjata;
+	public String getSenjata(){ //menginisialisasi nilai Senjata
+		return Senjata; //mengembalikan nilai Senjata
 	}
-	public void setSenjata(String d){
+	public void setSenjata(String d){ //mendeklarasikan set untuk atribut Senjata
 		this.Senjata=d;
 	}
 	
 	public static void main(String [] args){
 		Jepang makananPertama = new Jepang();
-		makananPertama.setMakanan("Sushi");
-		System.out.print("Salah Satu Makanan Asal Jepang Adalah: ");
-		System.out.println(makananPertama.getMakanan());
+		makananPertama.setMakanan("Sushi"); //mengisi nilai setter Makanan
+		System.out.print("Salah Satu Makanan Asal Jepang Adalah: "); //output kalimat
+		System.out.println(makananPertama.getMakanan()); //memanggil setter Makanan dengan getter
 	
-		makananPertama.setPakaian("Kimono");
-		System.out.print("Pakaian Asal Jepang Adalah: ");
-		System.out.println(makananPertama.getPakaian());
+		makananPertama.setPakaian("Kimono"); //mengisi nilai setter pakaian
+		System.out.print("Pakaian Asal Jepang Adalah: "); //output kalimat
+		System.out.println(makananPertama.getPakaian()); //memanggil setter Pakaian dengan getter
 		
-		makananPertama.setBunga("Sakura");
-		System.out.print("Bunga Asal Jepang Adalah: ");
-		System.out.println(makananPertama.getBunga());
+		makananPertama.setBunga("Sakura"); //mengisi nilai setter Bunga
+		System.out.print("Bunga Asal Jepang Adalah: "); //output kalimat
+		System.out.println(makananPertama.getBunga()); //memanggil setter Bunga dengan getter
 		
-		makananPertama.setSenjata("Katana");
-		System.out.print("Senjata Asal Jepang Adalah: ");
-		System.out.println(makananPertama.getSenjata());
+		makananPertama.setSenjata("Katana"); //mengisi nilai setter Senjata
+		System.out.print("Senjata Asal Jepang Adalah: "); //output kalimat
+		System.out.println(makananPertama.getSenjata()); //memanggil setter Senjata dengan getter
 	}
 }
-
-
